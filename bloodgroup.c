7a6493f952e58ca88d3e96f729a3fed0580c0dda
ssrttt <saritasaisema@gmail.com>
2021-09-16 22:13:48 +0700

    add bloodgroup B

--- bloodgroup.c
+++ bloodgroup.c
@@ -33,8 +33,8 @@ void main()
     }
     else if (strcmp(G, "AB") == 0)
     {
-      printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
-      printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
+      printf("%s, AB. OMG, you can give blood only to: AB.\n", Name);
+      printf("  Oh, you can receive blood from all: O, A, B, AB.\n");
     }
     else
     {
